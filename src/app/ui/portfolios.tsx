0937133e528a584a13c34f8ccce59b38cc0b6fd6
MinJunKimsdaads <aaeexx@naver.com>
2024-03-20 14:05:44 +0900

    20240320

---
+++ src/app/ui/portfolios.tsx
@@ -0,0 +1,16 @@
+function PortfolioItem(){
+    return (
+        <div>
+            Portfoio
+        </div>
+    )    
+}
+
+export function PortfolioWrapper(){
+    return (
+        <div>
+            <PortfolioItem></PortfolioItem>
+        </div>
+    )
+}
+
